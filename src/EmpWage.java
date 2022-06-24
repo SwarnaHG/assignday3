a2bc5ee48e3384032ca069ba53af71589cb9668c
SwarnaHG <swarnahg18@gmail.com>
2022-06-24 23:37:26 +0530

    Using Switch case

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -8,23 +8,18 @@ public class EmpWage {
         int wagePerHour = 20;
         int numberOfHours = 8;
         int numberOfHoursForPartTime = 4;
-        int workingDayInAMonth = 20;
         int isPresent = (int) Math.floor(Math.random() * 3);
         switch (isPresent) {
             case 0:
                 System.out.println("Employee is Present");
                 System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-                System.out.println("Employee earn per Month =" + numberOfHours * wagePerHour * workingDayInAMonth);
                 break;
             case 1:
                 System.out.println("Employee is doing Part Time");
                 System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
-                System.out.println("Employee earn per Month =" + numberOfHoursForPartTime * wagePerHour * workingDayInAMonth);
-
                 break;
             case 2:
                 System.out.println("Employee is not Present");
-                System.out.println("Employee earn = 0");
                 System.out.println("Employee earn = 0");
                 break;
         }
