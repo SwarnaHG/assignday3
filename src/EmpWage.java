5e1b16c2a6a92f2937f63805ce2561063470d073
SwarnaHG <swarnahg18@gmail.com>
2022-06-24 23:43:39 +0530

    Attendance

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,24 +1,16 @@
+package empwage;
+
 public class EmpWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
         // TODO Auto-generated method stub
         int employeePresent = 1;
-        int employeePartTime = 2;
-        int wagePerHour = 20;
-        int numberOfHours = 8;
-        int numberOfHoursForPartTime = 4;
-        int isPresent = (int) Math.floor(Math.random() * 3);
-        if (isPresent == employeePresent) {
+        int isPresent = (int) (Math.floor(Math.random() * 10) % 2);
+        if (isPresent == employeePresent)
             System.out.println("Employee is Present");
-            System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-        } else if (isPresent == employeePartTime) {
-            System.out.println("Employee is doing Part Time");
-            System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
-        }
-        else {
+        else
             System.out.println("Employee is not Present");
-            System.out.println("Employee earn = 0");
-        }
     }
+
 }
